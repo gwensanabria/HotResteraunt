29fa97ea2c19dd276d50192921a6af70e7fffe97
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-07-06 20:37:25 -0500

    uncomment

--- index.js
+++ index.js
@@ -1,72 +1,54 @@
-
 // Dependencies
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
 // set up Express
-const app = express()
-const PORT = 3000
-
+const app = express();
+const PORT = 3000;
 
 // DATA
 
-const reserved = []
+const reserved = [];
 
-const waiting = []
+const waiting = [];
 
 const reservation = [
-    {
-        customerName: 'Link',
-        customerEmail: 'link@email.com',
-        customerID: '1234',
-        phoneNumber: '123-456-7890',
+  {
+    customerName: "Link",
+    customerEmail: "link@email.com",
+    customerID: "1234",
+    phoneNumber: "123-456-7890",
+  },
+  {
+    customerName: "Zelda",
+    customerEmail: "zelda@email.com",
+    customerID: "1234",
+    phoneNumber: "123-456-7890",
+  },
+  {
+    customerName: "Ganon",
+    customerEmail: "ganon@email.com",
+    customerID: "1234",
+    phoneNumber: "123-456-7890",
+  },
+];
 
-    },
-    {
-        customerName: 'Zelda',
-        customerEmail: 'zelda@email.com',
-        customerID: '1234',
-        phoneNumber: '123-456-7890',
-
-    },
-    {
-        customerName: 'Ganon',
-        customerEmail: 'ganon@email.com',
-        customerID: '1234',
-        phoneNumber: '123-456-7890',
-=======
-app.listen(port, function() {
-    console.log("Listening at PORT " + port);
+//Direct to homepage
+app.get("/", function (req, res) {
+  res.sendFile(path.join(_dir, "home.html"));
 });
 
-//Reservation Questions
-
-const reserved = []
-
-    }
-]
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    //directs tp the home page
-    app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-  
-  //directs to the reservation page
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
-  });
-  
-  //directs to the tables page
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"));
-  });
-
+//directs to the reservation page
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+//directs to the tables page
+app.get("/tables", function (req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
 
 // Server listening
-app.listen(PORT, function() {
-    console.log('App listening on PORT' + port)
-})
+app.listen(PORT, function () {
+  console.log("App listening on PORT" + PORT);
+});
